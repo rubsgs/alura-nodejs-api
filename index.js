eaae1138e4f2e354e29eaafeb5e19455b1dc274b
rubens guarnieri <rgs.rubensguarnieri@gmail.com>
2021-08-09 12:14:43 -0300

    Aula 02, separaçao da configuraçao do servidor com o consign, criaçao da pasta 'config', criaçao da rota de POST

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const express = require('express');
-const app = express();
+const customExpress = require('./config/custom-express');
+app = customExpress();
+
 app.listen(3000, () => {
     console.log(`servidor em execução`);
-});
-
-app.get('/atendimentos', (req, res) => {
-    res.send('Você está na rota de atendimentos e está realizando um GET');
 });
